1655f25229c091eb9b74863143cca94c9a366767
kamruzzamanripon <rkamruzzaman@gmail.com>
2022-12-21 04:16:34 +0600

    best seller add

--- components/index.js
+++ components/index.js
@@ -1,8 +1,9 @@
 /* eslint-disable import/no-cycle */
 import Banner from './Banner';
 import Button from './Button';
+import CreatorCard from './CreatorCard';
 import Footer from './Footer';
 import Navbar from './Navbar';
 
-export { Navbar, Footer, Button, Banner };
+export { Navbar, Footer, Button, Banner, CreatorCard };
 
