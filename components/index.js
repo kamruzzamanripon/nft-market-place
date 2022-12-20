6ed3e864cdc32a28f65538152fdc615076e734c7
kamruzzamanripon <rkamruzzaman@gmail.com>
2022-12-21 02:32:02 +0600

    make body

---
+++ components/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-cycle */
+import Banner from './Banner';
+import Button from './Button';
+import Footer from './Footer';
+import Navbar from './Navbar';
+
+export { Navbar, Footer, Button, Banner };
+
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import { ThemeProvider } from 'next-themes';
+import Script from 'next/script';
+import { Footer, Navbar } from '../components';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white ">
+      <Navbar />
+      <div className="pt-65">
+        <Component {...pageProps} />
+      </div>
+      <Footer />
+    </div>
 
-export default MyApp
+    <Script src="https://kit.fontawesome.com/9f17f34755.js" crossorigin="anonymous" />
+  </ThemeProvider>
+);
+
+export default MyApp;
+
